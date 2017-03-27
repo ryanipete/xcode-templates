7b5ca07f186fff17c7f58425f735d53c38fe520f
Ryan Peterson <rpeterson@housecanary.com>
2017-03-27 11:08:28 -0600

    Customize template

--- Custom Unit Test Case Class.xctemplate/XCTestCaseSwift/___FILEBASENAME___.swift
+++ Custom Unit Test Case Class.xctemplate/XCTestCaseSwift/___FILEBASENAME___.swift
@@ -6,30 +6,37 @@
 //___COPYRIGHT___
 //
 
+@testable import ___VARIABLE_testedModule___
 import XCTest
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_testSubclass___ {
 
+    // MARK: Properties
+
+    var subject: ___VARIABLE_productName___!
+
+    // MARK: Setup and Teardown
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        subject = ___VARIABLE_productName___()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // MARK: Tests
+
+    func testThat_() {
+
+        // given
+
+        // when
+
+        // then
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    // MARK: Helpers
 
 }
